2672e286a58f4a861624f9fa47bebe42997d36d6
barry108 <61048276+barry108@users.noreply.github.com>
2020-02-14 21:52:08 +1100

    Add files via upload

---
+++ Jupiter notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#This is for Capstone use
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
